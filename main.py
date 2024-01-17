f1287d1d4ef23a2e8f7b2c02a2fa6585de8ebea5
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-01-16 21:03:34 -0300

    fix: datetime format invalid

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-# main.py
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from mrr import calculate_simple_mrr
+import pandas as pd
+import io
+import csv
+from openpyxl import load_workbook
+   
 app = FastAPI()
+
+origins = [
+   'http://localhost:3000'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -10,3 +28,33 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
+
+
+## TODO: protect API if a KEY
+## TODO: unit test for this route
+
+@app.post("/upload-file/")
+async def upload_file(file: UploadFile = File(...)):
+  try:
+    extension = file.filename.split('.')[-1]
+    data = await file.read();
+  
+    if extension == 'csv':
+      result = calculate_simple_mrr(io.StringIO(data.decode('utf-8')))
+      return JSONResponse(content=result)
+    
+    elif extension == 'xlsx':
+      workbook = load_workbook(io.BytesIO(data))
+      sheet = workbook.active
+      csv_data = io.StringIO()
+      csv_writer = csv.writer(csv_data)
+
+      for row in sheet.iter_rows(min_row=1, values_only=True):
+        csv_writer.writerow(row)
+
+      result = calculate_simple_mrr(io.StringIO(csv_data.getvalue()))
+      return JSONResponse(content=result)
+
+
+  except Exception as e:
+      raise HTTPException(status_code=500, detail=f"Falha ao processar o arquivo: {str(e)}")
---
+++ mrr.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import csv
+import io
+from datetime import datetime
+  
+def date_converter(data_str):
+    try:
+        # Tenta converter usando o formato do arquivo XLSX
+        return datetime.strptime(data_str, '%Y-%m-%d %H:%M:%S')
+    except ValueError:
+        try:
+            # Tenta converter usando o formato do arquivo CSV
+            return datetime.strptime(data_str, '%m/%d/%y %H:%M')
+        except ValueError:
+            # Se nenhum dos formatos for correspondido, levanta um ValueError
+            raise ValueError(f"Formato de data não suportado: {data_str}")
+
+
+def calculate_simple_mrr(data):
+  active_subscribers = 0
+  annual_subscriptions = 0
+  monthly_subscriptions = 0
+  monthly_billing = 0
+  annual_billing = 0
+
+  csv_reader = csv.DictReader(data)
+
+  for row in csv_reader:
+    initial_date = date_converter(row['data início'])
+
+    if row['status'] == 'Ativa' and (row['data cancelamento'] == '' or datetime.strptime(row['data cancelamento'], '%m/%d/%y %H:%M') > initial_date):
+      active_subscribers += 1
+      value = float(row['valor'].replace(',', '.'))
+
+      if row['periodicidade'] == 'Anual':
+        annual_billing += value
+        annual_subscriptions += 1
+      
+      if row['periodicidade'] == 'Mensal':
+        monthly_billing += value
+        monthly_subscriptions += 1
+  
+  return {
+    "active_subscribers": active_subscribers,
+    "annual_subcriptions": annual_subscriptions,
+    "monthly_subscriptions": monthly_subscriptions,
+    "monthly_billing": round(monthly_billing, 2),
+    "annual_billing": round(annual_billing, 2)
+  };
